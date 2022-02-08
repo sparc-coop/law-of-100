cbcc9060c3b7dbfd22b509c0f1af1279ea2d408b
Joshua Parker <joshua4_4@hotmail.com>
2022-02-08 14:33:16 +0000

    Feature updates

---
+++ LawOf100.Web/Features/Entities/Progression.cs
@@ -0,0 +1,34 @@
+﻿using Sparc.Core;
+
+namespace LawOf100.Features.Features.Entities
+{
+    public class Progression : Root<string>
+    {
+        private Progression()
+        {
+            Id = Guid.NewGuid().ToString();
+
+            //The day the user is on
+            Day = 1;
+
+            //States of the user's days tacked
+            CurrentDay = "CurrentDay";
+            FutureDay = "FutureDay";
+            PassedDay = "PassedDay";
+            CompleteDay = "CompleteDay";
+            FailedDay = "FailedDay";
+        }
+
+        public Progression(int day) : this()
+        {
+            Day = day;
+        }
+
+        public int Day { get; set; }
+        public string CurrentDay { get; set; }
+        public string FutureDay { get; set; }
+        public string PassedDay { get; set; }
+        public string CompleteDay { get; set; }
+        public string FailedDay { get; set; }
+    }
+}
---
+++ LawOf100.Web/Features/TrackDay.cs
@@ -0,0 +1,26 @@
+﻿using LawOf100.Features.Features.Entities;
+using Sparc.Core;
+using Sparc.Features;
+
+namespace LawOf100.Features.Features
+{
+    public class TrackDay : PublicFeature<int, string>
+    {
+        public TrackDay(IRepository<Progression> days)
+        {
+            Days = days;
+        }
+
+        public IRepository<Progression> Days { get; }
+
+        public override async Task<string> ExecuteAsync(int currentday)
+        {
+            var day = new Progression (currentday);
+            await Days.AddAsync(day);
+
+            day = Days.Query.FirstOrDefault(x => x.Day == 1 /*currentday*/);
+
+            return day.Id;
+        }
+    }
+}
