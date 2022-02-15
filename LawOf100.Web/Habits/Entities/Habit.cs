07a375164c76cb1e5e3cb39147601d42e4466096
Joseph Young <joseph@josephayoung.com>
2022-02-15 17:38:15 -0600

    Initial thoughts on recurrence

--- LawOf100.Web/Habits/Entities/Habit.cs
+++ LawOf100.Web/Habits/Entities/Habit.cs
@@ -9,14 +9,16 @@ public class Habit : Root<string>
         UserId = string.Empty;
         Id = Guid.NewGuid().ToString();
         HabitName = "Stop Smoking";
-        RecurrenceOptions = new Recurrence();
+        StartDate = DateTime.UtcNow;
+        RecurrenceOptions = new Recurrence(0, 0);
         Progressions = new List<Progression>();
     }
 
-    public Habit(string userId, string habitName) : this()
+    public Habit(string userId, string habitName, int repeatEveryXHours) : this()
     {
         UserId = userId;
         HabitName = habitName;
+        RecurrenceOptions = new Recurrence(repeatEveryXHours, 0.5);
     }
 
     internal void TrackProgress(int day, bool? isSuccessful, decimal rating, string? review)
@@ -24,9 +26,10 @@ public class Habit : Root<string>
         throw new NotImplementedException();
     }
 
-    public string UserId { get; set; }
-    public string HabitName { get; set; }
-    public List<Progression> Progressions { get; set; }
+    public string UserId { get; private set; }
+    public string HabitName { get; private set; }
+    public DateTime StartDate { get; private set; }
+    public List<Progression> Progressions { get; private set; }
 
-    public Recurrence RecurrenceOptions { get; set; }
+    public Recurrence RecurrenceOptions { get; private set; }
 }
--- LawOf100.Web/Habits/Entities/Recurrence.cs
+++ LawOf100.Web/Habits/Entities/Recurrence.cs
@@ -2,4 +2,17 @@
 
 public class Recurrence
 {
+    public Recurrence(int repeatEveryXHours, double fudgeFactor)
+    {
+        RepeatEveryXHours = repeatEveryXHours;
+        FudgeFactor = fudgeFactor;
+    }
+
+    public int RepeatEveryXHours { get; }
+    public double FudgeFactor { get; }
+
+    public bool IsDayMissed(DateTime lastDayTracked)
+    {
+        return lastDayTracked.AddHours(RepeatEveryXHours * FudgeFactor) > DateTime.UtcNow;
+    }
 }
