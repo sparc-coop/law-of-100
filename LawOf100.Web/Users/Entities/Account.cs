610e9e1e86ab17155ebde313d3d9e3c19838df58
Laura Ferraz <lauraferraz5@gmail.com>
2022-02-23 13:41:10 -0300

    Added ShortIntro to Account object and save during CreateAccount

--- LawOf100.Web/Users/Entities/Account.cs
+++ LawOf100.Web/Users/Entities/Account.cs
@@ -8,17 +8,20 @@ public class Account : Root<string>
     {
         Id = Guid.NewGuid().ToString();
         Name = "";
+        ShortIntro = "";
         DateCreated = DateTime.UtcNow;
         DateModified = DateTime.UtcNow;
     }
 
-    public Account(string id, string name) : this()
+    public Account(string id, string name, string? intro) : this()
     {
         Id = id;
         Name = name;
+        ShortIntro = intro;
     }
 
     public string Name { get; set; }
+    public string? ShortIntro { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime DateModified { get; set; }
     public string UserId { get { return Id; } set { Id = value; } }
