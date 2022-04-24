6eebd1baa845b1b41b0ab85ef60d7eafbafc398d
Joseph Young <joseph@josephayoung.com>
2022-04-24 10:43:47 -0500

    Gtag.js was refreshing every page

--- LawOf100.Webb/Program.cs
+++ LawOf100.Webb/Program.cs
@@ -1,3 +1,4 @@
+using Blazor.Analytics;
 using LawOf100.Features;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sparc.Platforms.Web;
@@ -15,6 +16,7 @@ public class Program
         builder.RootComponents.Add<App>("#app");
 
         builder.Services.AddScoped<IConfiguration>(_ => builder.Configuration);
+        builder.Services.AddGoogleAnalytics("G-FQ2LH8DGH3");
 
         builder.AddB2CApi<LawOf100Api>(
             "https://lawof100.onmicrosoft.com/9a4ea546-b5c9-4d27-918e-1029e60f7089/LawOf100.Access",
