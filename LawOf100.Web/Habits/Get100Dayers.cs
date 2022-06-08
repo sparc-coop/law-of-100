308def1718157a71b39644de54aee5bdee06d9f8
Laura Ferraz <lauraferraz5@gmail.com>
2022-06-08 11:40:33 -0300

    Created "Previous 100 Dayers" Feature

--- LawOf100.Web/Habits/Get100Dayers.cs
+++ LawOf100.Web/Habits/Get100Dayers.cs
@@ -5,7 +5,7 @@ using Sparc.Features;
 
 namespace LawOf100.Features.Habits;
 
-public record Get100DayersResponse(string HabitName, string Username, DateTime ActualDate, string? Review);
+public record Get100DayersResponse(string HabitName, string? Username, DateTime ActualDate, string? Review);
 public class Get100Dayers : PublicFeature<List<Get100DayersResponse>>
 {
     public Get100Dayers(IRepository<Habit> habits, IRepository<Account> users)
@@ -19,24 +19,40 @@ public class Get100Dayers : PublicFeature<List<Get100DayersResponse>>
 
     public override async Task<List<Get100DayersResponse>> ExecuteAsync()
     {
+        var habits = await Habits.Query
+            .Where(x => x.IsCompleted == true)
+            .OrderByDescending(x => x.LastTrackedDate)
+            .Take(5)
+            .ToListAsync();
 
-        //var habits = await Habits.Query.Where(x => x.Progressions.Any(y => y.Day == 100 && y.IsSuccessful == true)).ToList();
+        var userIds = habits.Select(x => x.UserId).Distinct().ToList();
+        var users = await Users.Query.Where(x => userIds.Contains(x.UserId)).ToListAsync();
 
-        var habits = await Habits.Query.SelectMany(x => x.Progressions.Where(y => y.Day == 100 && y.IsSuccessful == true)).ToListAsync();
+        var timelineEntries = new List<Get100DayersResponse>();
 
+        foreach (var habit in habits)
+        {
+            foreach (var progression in habit.GetTimeline().Where(x => x.IsPublic == true))
+            {
+                var entry = new Get100DayersResponse(
+                    habit.HabitName,
+                    UsernameForId(users, habit.UserId),
+                    progression.ActualDate!.Value,
+                    progression.Review);                                 
+                timelineEntries.Add(entry);
+            }
+        }
 
-        return null;
+        var timelines = timelineEntries
+            .OrderByDescending(x => x.ActualDate)
+            .Select(x => new Get100DayersResponse(
+                HabitName: x.HabitName,
+                Username: x.Username,
+                ActualDate: x.ActualDate,
+                Review: x.Review))                
+            .ToList();
 
-        //var dayers = habits
-        //    .Select(x => new Get100DayersResponse(
-        //        HabitName: x.HabitName,
-        //        Username: x.Username,
-        //        ActualDate: x.ActualDate,
-        //        Review: x.Review
-        //        ))
-        //    .ToList();
-
-        //return dayers;
+        return timelines;
     }
 
     string? UsernameForId(List<Account> users, string id)
