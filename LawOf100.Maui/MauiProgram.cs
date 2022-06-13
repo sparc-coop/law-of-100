2cef6ba6cd2eee5b091c5a2451fc5bcb943959da
Joseph Young <joseph@josephayoung.com>
2022-06-13 09:29:04 -0500

    progress

---
+++ LawOf100.Maui/MauiProgram.cs
@@ -0,0 +1,18 @@
+﻿using LawOf100.UI.Shared;
+using Sparc.Platforms.Maui;
+
+namespace LawOf100.Maui;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder().Sparcify<MainLayout>();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+        return builder.Build();
+    }
+}
---
+++ LawOf100.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LawOf100.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
